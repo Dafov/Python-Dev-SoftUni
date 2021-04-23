80d41bf08cc665edc74507ed3148d57bac00cd72
Georgi Dafov <gs.dafov@gmail.com>
2021-04-24 00:18:38 +0300

    Basic-syntax-Conditional-statements-and-Loops

---
+++ 02 . Programing-Fundamentals-with-Python/01. Basic-syntax-Conditional-statements-and-Loops/03-word-reverse.py
@@ -0,0 +1,7 @@
+s = input()
+reversed_s = ""
+
+for c in reversed(s):
+    reversed_s += c
+
+print(reversed_s)
---
+++ 02 . Programing-Fundamentals-with-Python/01. Basic-syntax-Conditional-statements-and-Loops/exercise/04. Double-char.py
@@ -0,0 +1,7 @@
+our_input = input()
+result = ""
+
+for ch in our_input:
+    result += (ch + ch)
+
+print(result)
